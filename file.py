a5bc4c2fb96957f6ae079485a66b2a314f41bb22
njambiEvelyne <evelynenganga76@gmail.com>
2026-02-03 11:39:18 +0300

    Added an exception

--- file.py
+++ file.py
@@ -161,3 +161,14 @@ else:
     print("Result is:", res)
 finally:
     print("Execution completed.")
+
+""""
+Catching multiple exceptions
+"""
+# a = ["10", "twenty", "30"]
+# try:
+#     total = int(a[0] + int a [1])
+# except (ValueError, TypeError) as e:
+#     print("Error occurred:", e)
+# except IndexError:
+#     print("Index out of range!")
