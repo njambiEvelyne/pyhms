09a753947df2c9236ad16abc39be57f2fe53cffb
njambiEvelyne <evelynenganga76@gmail.com>
2026-02-06 09:35:46 +0300

    Added new file

--- proj/couter.py
+++ proj/couter.py
@@ -1,6 +1,9 @@
 num = int(input("Enter 10 numbers: "))
 for i in num:
-    if i % 2 == 0:
-        print(f"{i} is an even number.")
-    elif i % 2 != 0:  
-        print(f"{i} is an odd number.")
+  total = []
+  if i % 2 == 0:
+    total.append(i)
+    count = len(total)
+print(f"There are {count} even numbers in the list.")
+    
+
