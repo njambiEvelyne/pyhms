f73051c3e273bf99629db492aa7969ddd400568a
njambiEvelyne <evelynenganga76@gmail.com>
2026-02-07 23:45:37 +0300

    Counter mapping correction

--- proj/couter.py
+++ proj/couter.py
@@ -1,3 +1,13 @@
-
+num = input("Enter 10 numbers separated by spaces: ")
+numbers = list(map(int, num.split()))
+if len(numbers) != 10:
+    print("Please enter exactly 10 numbers")
+else:
+    total = []
+    for i in numbers:
+        if i % 2 == 0:
+            total.append(i)
+    count = len(total)
+    print(f"There are {count} even numbers in the list.")
     
 
