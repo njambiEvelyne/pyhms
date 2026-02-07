21fdc878c39320d2dbd5de5717c59530f80aa305
njambiEvelyne <evelynenganga76@gmail.com>
2026-02-07 23:43:32 +0300

    Deleted code

--- proj/couter.py
+++ proj/couter.py
@@ -1,9 +1,3 @@
-num = int(input("Enter 10 numbers: "))
-for i in num:
-  total = []
-  if i % 2 == 0:
-    total.append(i)
-    count = len(total)
-print(f"There are {count} even numbers in the list.")
+
     
 
