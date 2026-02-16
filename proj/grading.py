d5513bf0f94909f4c92773dedc15b36b026f8c03
njambiEvelyne <evelynenganga76@gmail.com>
2026-02-16 12:02:24 +0300

    Added a grading scale

--- proj/grading.py
+++ proj/grading.py
@@ -2,5 +2,12 @@ name = input("Enter the student's name: ")
 grade = int(input("Enter the student's grade (0-100): "))
 
 if grade >= 90:
+  print(f"{name} received an A.")
+elif grade >= 80:
+  print(f"{name} received a B.")  
+elif grade >= 70:
+  print(f"{name} received a C.")
+
+
 
 
